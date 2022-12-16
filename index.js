f761869dadfccc2a4379aa22b5cbc5934dce569a
Daniel <muchiridaniel59@gmail.com>
2022-12-16 00:40:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 // Code your solutions in this file
-for (let age = 30; age < 40; age++) {
-    console.log(`I'm ${age} years old. Happy birthday to me!`);
-    debugger;
-  }
-  let string_name = ["Kev", "Dan", "Ann",];
-  let event_name = "bash";
-  function writeCards(string_name, event_name) {
-    var array_store = [];
-    for (let counter = 0; counter < string_name.length; counter++){
-        array_store.push(`Thank you, ${string_name[counter]}, for the wonderful ${event_name} gift!`);
-    }
-    return array_store;
-  }
+const spy = ['Guadalupe','Ollie', 'Aki'];
+function  writeCards(spy){
+    
+    const surprise = []
+    for(let i=0; i < spy.length ;  i++){
+       
+        surprise.push (`Thank you, ${spy[i]}, for the wonderful surprise gift!`)
+        console.log( surprise);
 
-  function countDown(integer) {
-    while (integer >= 0) {
-        console.log(integer)
     }
-  }
+    
+    return surprise;
+}
+function countDown(){
+    for (let j = 10; j>=0; j--){
+        console.log(j)
+
+    }
+    
+
+}
+countDown()
